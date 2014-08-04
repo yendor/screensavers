ea3a9d35410cc580b3ac552354d8bd9349e33cf0
Rodney Amato <Rodney.Amato@overthewire.com.au>
2014-08-04 19:16:12 +1000

    Added the ability to draw a grid of squares on the screen and vary their colour over time

--- square/square.go
+++ square/square.go
@@ -7,9 +7,14 @@ import (
 )
 
 var (
-  width, height    int
-  centerX, centerY float32
+    width, height    int
+    centerX, centerY float32
 )
+
+type color struct {
+    red, green, blue float32
+}
+
 
 func errorCallback(err glfw.ErrorCode, desc string) {
     fmt.Printf("%v: %v\n", err, desc)
@@ -35,10 +40,14 @@ func main() {
     width = monitorMode.Width;
     height = monitorMode.Height;
 
-    window, err := glfw.CreateWindow(monitorMode.Width, monitorMode.Height, "Testing", nil, nil)
+    width = 1024
+    height = 1024
+
+    window, err := glfw.CreateWindow(width, height, "Testing", nil, nil)
     if err != nil {
         panic(err)
     }
+
 
     window.SetFramebufferSizeCallback(framebufferSizeCallback)
 
@@ -47,57 +56,89 @@ func main() {
     for !window.ShouldClose() {
         //Do OpenGL stuff
         drawScene(window);
-
-
-
         window.SwapBuffers()
         glfw.PollEvents()
     }
 }
 
 func framebufferSizeCallback(window *glfw.Window, width, height int) {
-  ratio := float64(width) / float64(height)
+    ratio := float64(width) / float64(height)
 
-  updateDimensions(window)
+    updateDimensions(window)
 
-  gl.Viewport(0, 0, width, height)
-  gl.Clear(gl.COLOR_BUFFER_BIT)
-  gl.MatrixMode(gl.PROJECTION)
-  gl.LoadIdentity()
-  gl.Ortho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
+    gl.Viewport(0, 0, width, height)
+    gl.Clear(gl.COLOR_BUFFER_BIT)
+    gl.MatrixMode(gl.PROJECTION)
+    gl.LoadIdentity()
+    gl.Ortho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
 
-  gl.MatrixMode(gl.MODELVIEW)
-  gl.LoadIdentity()
+    gl.MatrixMode(gl.MODELVIEW)
+    gl.LoadIdentity()
 }
 
 
 func updateDimensions(window *glfw.Window) {
-  width, height = window.GetFramebufferSize()
-  centerX = float32(width) / 2
-  centerY = float32(height) / 2
+    width, height = window.GetFramebufferSize()
+    centerX = float32(width) / 2
+    centerY = float32(height) / 2
 }
 
 func drawScene(window *glfw.Window) {
-  gl.Clear(gl.COLOR_BUFFER_BIT)
-  gl.LoadIdentity()
+    gl.Clear(gl.COLOR_BUFFER_BIT)
+    gl.LoadIdentity()
 
-  // Read timer since glfw.Init()
-  time := glfw.GetTime()
+    // Read timer since glfw.Init()
+    time := glfw.GetTime()
 
-  gl.Rotatef(float32(time)*50.0, -0.2, 0.0, 1.0)
-  /* gl.Translatef(-0.9, 0, 0)*/
+    // width, height = window.GetFramebufferSize()
+    // ratio := float64(width) / float64(height)
 
-  gl.Begin(gl.QUADS)
-  gl.Color3f(1.0, 0.0, 0.0)
-  gl.Vertex3f(-0.5, -0.5, 0.0)
+    // gl.Rotatef(float32(time)*50.0, -0.2, 0.0, 1.0)
+    // multiplier := float32(1.0)
+    // left := float32(time)
+    // if left >= float32(ratio) {
+    //     multiplier = - 0.1
+    // }
 
-  gl.Color3f(0.0, 1.0, 0.0)
-  gl.Vertex3f(0.5, -0.5, 0.0)
+    // if left <= float32(-ratio) {
+    //     multiplier = 0.1
+    // }
 
-  gl.Color3f(0.0, 0.0, 1.0)
-  gl.Vertex3f(0.5, 0.5, 0.0)
+    // gl.Translatef(left * multiplier, 0, 0)
 
-  gl.Color3f(0.0, 0.0, 1.0)
-  gl.Vertex3f(-0.5, 0.5, 0.0)
-  gl.End()
+    var left, top, width float32
+    var i, j float32
+
+
+    left = -1
+    top = -1
+    width = 0.5
+
+
+    for j = 0; j < 4; j++ {
+        for i = 0; i < 4; i++ {
+            c := color{1.0/(i+1) * float32(time), 1.0/(j+1) * float32(time), 0.5 * float32(time)}
+            drawSquare(left + width * i, top + width * j, width, c)
+        }
+    }
+}
+
+func drawSquare(left float32, top float32, width float32, c color) {
+    gl.Begin(gl.QUADS)
+
+    gl.Color3f(c.red, c.green, c.blue)
+
+    // bottom left
+    gl.Vertex3f(left, top, 0.0)
+
+    // top left
+    gl.Vertex3f(left, top + width, 0.0)
+
+    // top right
+    gl.Vertex3f(left + width, top + width, 0.0)
+
+    // bottom right
+    gl.Vertex3f(left + width, top, 0.0)
+
+    gl.End()
 }
