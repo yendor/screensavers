0a2c4afc53f814b4735fd2a77fcfdfba72122215
Rodney Amato <Rodney.Amato@overthewire.com.au>
2014-08-06 08:11:18 +1000

    Use the distance from a centre point to determine the colour of the square

--- 8bit_fire/8bit_fire.go
+++ 8bit_fire/8bit_fire.go
@@ -1,65 +1,144 @@
 package main
 
 import (
-    "github.com/go-gl/gl"
-    glfw "github.com/go-gl/glfw3"
-    "math"
+	"github.com/go-gl/gl"
+	glfw "github.com/go-gl/glfw3"
+	"math"
+	// "log"
 )
 
 func drawScene(window *glfw.Window) {
-    gl.Clear(gl.COLOR_BUFFER_BIT)
-    gl.LoadIdentity()
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gl.LoadIdentity()
 
-    // Read timer since glfw.Init()
-    // time := glfw.GetTime()
+	// Read timer since glfw.Init()
+	// time := glfw.GetTime()
 
-    // width, height = window.GetFramebufferSize()
-    // ratio := float64(width) / float64(height)
+	// width, height = window.GetFramebufferSize()
+	// ratio := float64(width) / float64(height)
 
-    // gl.Rotatef(float32(time)*50.0, -0.2, 0.0, 1.0)
+	// gl.Rotatef(float32(time)*50.0, -0.2, 0.0, 1.0)
 
-    // gl.Translatef(left * multiplier, 0, 0)
+	// gl.Translatef(left * multiplier, 0, 0)
 
-    var x, y, width float32
-    var i, j float32
-    var c color
+	var x, y, width float32
+	var i, j float32
+	var c color
 
-    x = -1
-    y = -1
-    width = 0.25
 
-    for j = 0; j < 8; j++ {
-        for i = 0; i < 8; i++ {
-            c = getColor(x + width * i, y + width * j)
+	y = -1
+	x = -1
+	width = 0.25
 
-            drawSquare(x + width * i, y + width * j, width, c)
-        }
-    }
+	for j = x; j < 1; j += width {
+		for i = y; i < 1; i += width {
+			c = getColor(j, i+1)
+			drawSquare(j, i, width, c)
+		}
+	}
+	x = -1
+	c = getColor(x, y+1)
+	drawSquare(x, y, width, c)
+
+	// x = -0.75
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = -0.5
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = -0.25
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.25
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.5
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.75
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 1
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+
+	// y = -0.75
+	// x = -0.75
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = -0.5
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = -0.25
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.25
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.5
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 0.75
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
+
+	// x = 1
+	// c = getColor(x, y+1)
+	// drawSquare(x, y, width, c)
 }
 
 // getColor determines the colour of a square at a
 // certain point x, y on our window and returns the color type
 func getColor(x, y float32) color {
-    white  := color{255,255,255}
-    yellow := color{243, 199, 3}
-    orange := color{240, 55, 30}
-    red    := color{183, 16, 47}
-    green  := color{0, 255, 0}
+	white := color{255, 255, 255}
+	yellow := color{243, 199, 3}
+	orange := color{240, 55, 30}
+	red := color{183, 16, 47}
+	green := color{0, 255, 0}
 
-    absX := math.Abs(float64(x))
-    absY := math.Abs(float64(x))
+	black := color{0,0,0}
 
-    maxAbs := math.Max(absX, absY)
+	colors := make([]color, 5)
 
-    if maxAbs >= 0.75 {
-        return red
-    } else if maxAbs > 0.5 {
-        return orange
-    } else if maxAbs > 0.0 {
-        return yellow
-    } else if maxAbs == 0 {
-        return white
-    } else {
-        return green
-    }
+	colors[0] = white
+	colors[1] = yellow
+	colors[2] = orange
+	colors[3] = red
+	colors[4] = green
+
+	distance := getDistance(0.0, 0.0, float64(x), float64(y))
+
+	colorIndex := int(math.Abs(distance*4))
+	if colorIndex > 4 {
+		return black
+	}
+	// log.Printf("%v -> %v\n", (x+y), colorIndex)
+	// log.Print(colorIndex)
+
+	// colorIndex := int(math.Mod(maxAbs, 5))
+	return colors[colorIndex]
+}
+
+func getDistance(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2 -x1, 2) + math.Pow(y2 - y1, 2))
 }
